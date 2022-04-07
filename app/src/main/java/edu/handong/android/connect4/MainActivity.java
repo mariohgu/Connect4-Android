f4ae6270281e44a64144173d377a3a9e7e70389b
handongmario <mario@handong.ac.kr>
2022-04-07 22:55:45 +0900

    settings imagebutton funcionality

--- app/src/main/java/edu/handong/android/connect4/MainActivity.java
+++ app/src/main/java/edu/handong/android/connect4/MainActivity.java
@@ -2,12 +2,16 @@ package edu.handong.android.connect4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
 
     RelativeLayout r1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +20,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //r1=findViewById(R.id.RelativeLayout1);
         //r1.setBackgroundResource(R.drawable.background);
+        ImageButton btnSettings = findViewById(R.id.settings);
+        btnSettings.setOnClickListener(view -> {
+            Intent settings = new Intent(this, Settings.class);
+            startActivity(settings);
+        });
+
     }
 }
