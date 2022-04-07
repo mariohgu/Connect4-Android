9cb3ce656d33600c698bd8b5a4057e29af0326b9
raxe <raxethebest@handong.ac.kr>
2022-04-07 08:05:02 +0100

    raxe

--- app/src/main/java/edu/handong/android/connect4/MainActivity.java
+++ app/src/main/java/edu/handong/android/connect4/MainActivity.java
@@ -3,12 +3,18 @@ package edu.handong.android.connect4;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout r1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_Connect4);
         setContentView(R.layout.activity_main);
+        //r1=findViewById(R.id.RelativeLayout1);
+        //r1.setBackgroundResource(R.drawable.background);
     }
 }
