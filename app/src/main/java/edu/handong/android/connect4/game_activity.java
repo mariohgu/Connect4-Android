9e6ccc112a99afb301e367555fd888919fc9cf0c
handongmario <mario@handong.ac.kr>
2022-04-24 10:09:07 +0900

    onTouchListener in boardgame

--- app/src/main/java/edu/handong/android/connect4/game_activity.java
+++ app/src/main/java/edu/handong/android/connect4/game_activity.java
@@ -5,7 +5,10 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class game_activity extends AppCompatActivity {
 
@@ -19,5 +22,101 @@ public class game_activity extends AppCompatActivity {
             Intent intent = new Intent(this, NewGame_Settings.class);
             startActivity(intent);}
         );
+        View column1 = findViewById(R.id.view1);
+        View column2 = findViewById(R.id.view2);
+        View column3 = findViewById(R.id.view3);
+        View column4 = findViewById(R.id.view4);
+        View column5 = findViewById(R.id.view5);
+        View column6 = findViewById(R.id.view6);
+        View column7 = findViewById(R.id.view7);
+
+        column1.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: message(1);
+                        break;
+                }
+                return true;
+            }
+
+        });
+
+        column2.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: message(2);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        column3.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: message(3);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        column4.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: message(4);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        column5.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: message(5);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        column6.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: message(6);
+                        break;
+                }
+                return true;
+            }
+        });
+
+        column7.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent event) {
+
+                switch (event.getAction()) {
+                    case MotionEvent.ACTION_DOWN: message(7);
+                        break;
+                }
+                return true;
+            }
+        });
+
+
+
+    }
+
+    protected void message (int a){
+        CharSequence text = "COLUMN "+a;
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(this,text,duration);
+        toast.show();
     }
 }
