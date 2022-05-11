0110ca1394789a2bab31eb8d28d616992f2ad92d
handongmario <mario@handong.ac.kr>
2022-05-12 01:19:42 +0900

    add the singleplayer

--- app/src/main/java/edu/handong/android/connect4/Connect4View.java
+++ app/src/main/java/edu/handong/android/connect4/Connect4View.java
@@ -17,20 +17,185 @@ import android.view.View;
 
 public class Connect4View extends View {
     private Path PBoard, PAIPiece, PHumanPiece;
-    private Paint Paint, CanvasPaint;
-    private Canvas Canvas;
-    private Bitmap Bitmap;
-    private MainActivity mActivity;
+    private Paint cPaint, CanvasPaint;
+    private Canvas cCanvas;
+    private Bitmap cBitmap;
+    private game_activity gActivity;
     private float endY;
+    private float endX;
     private float cWidth;
+    private float radius;
+    private float distance;
     //Margin of the board(constants)
-    private static final float MARGINX = 20.0f;
-    private static final float MARGINY = 210.0f;
+    private static final float MARGINX = 450.0f;
+    private static final float MARGINY = 140.0f;
 
     public Connect4View(Context context, AttributeSet attrs) {
         super(context, attrs);
-        mActivity = (MainActivity) context;
-        //setPathPaint();
+        gActivity = (game_activity) context;
+        setPathPaint();
+    }
+    //Set the attributes of the figures using the Path class
+    private void setPathPaint() {
+        PBoard = new Path();
+        PAIPiece = new Path();
+        PHumanPiece = new Path();
+        cPaint = new Paint();
+        cPaint.setAntiAlias(true);
+        cPaint.setStrokeWidth(12);
+        cPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        cPaint.setStrokeJoin(Paint.Join.ROUND);
+        CanvasPaint = new Paint(Paint.DITHER_FLAG);
     }
 
-}
+    // Settings the size of the figures and set the each pixel is stored on 4 bytes.
+    @Override
+    protected void onSizeChanged(int w, int h, int o_w, int o_h) {
+        super.onSizeChanged(w, h, o_w, o_h);
+        cBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        cCanvas = new Canvas(cBitmap);
+    }
+    //OnDraw Class to create the figures
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawBitmap(cBitmap, 0, 0, CanvasPaint);
+        cWidth = 75.0f;
+        endY = canvas.getHeight()-MARGINY;
+        endX = canvas.getWidth()-MARGINX+120.0f;
+        radius= 20.0f;
+        distance = 2.0f;
+        // create the vertical lines of the board
+        for (int i=0; i<8; i++) {
+            float x = MARGINX + i * cWidth;
+            PBoard.moveTo(x, MARGINY);
+            PBoard.lineTo(x, canvas.getHeight()-MARGINY);
+        }
+        // create the horizontal line of the board (base)
+        PBoard.moveTo(MARGINX, canvas.getHeight() - MARGINY);
+        PBoard.lineTo(MARGINX + 7 * cWidth, canvas.getHeight() - MARGINY);
+        cPaint.setColor(0xFF0000FF);
+        canvas.drawPath(PBoard, cPaint);
+
+        //array to set the columns of the board
+        int columnPieces[] = {0,0,0,0,0,0,0};
+
+        //Conditional to create the pieces of the player or AI
+
+        if (gActivity.getAIFirst()) { //If the AI start
+            for (int i=0; i<gActivity.getAIMoves().size(); i++) {
+                int action = gActivity.getAIMoves().get(i);
+                int column = action % 7;
+                float x = MARGINX + column * cWidth + cWidth / 2.0f;
+                float y = canvas.getHeight()-MARGINY-cWidth*columnPieces[column]-cWidth/distance;
+                PAIPiece.addCircle(x,y, radius, Path.Direction.CW);
+                cPaint.setColor(0xFFFF0000);
+                canvas.drawPath(PAIPiece, cPaint);
+                columnPieces[column]++;
+
+                if (i<gActivity.getHumanMoves().size()) { // The player turn
+                    action = gActivity.getHumanMoves().get(i);
+                    column = action % 7;
+                    x = MARGINX+ column * cWidth + cWidth / 2.0f;
+                    //y = distance;
+                    y = canvas.getHeight()-MARGINY-cWidth*columnPieces[column]-cWidth/distance;
+                    PHumanPiece.addCircle(x,y, radius, Path.Direction.CW);
+                    cPaint.setColor(0xFFFFFF00);
+
+                    canvas.drawPath(PHumanPiece, cPaint);
+
+                    columnPieces[column]++;
+                }
+            }
+        }
+        else { //if the human player starts.
+            for (int i=0; i<gActivity.getHumanMoves().size(); i++) {
+                int action = gActivity.getHumanMoves().get(i);
+                int column = action % 7;
+                float x = MARGINX + column * cWidth + cWidth / 2.0f;
+                //float y = distance;
+                float y = canvas.getHeight()-MARGINY-cWidth*columnPieces[column]-cWidth/distance;
+                PHumanPiece.addCircle(x,y, radius, Path.Direction.CW);
+                cPaint.setColor(0xFFFFFF00);
+                canvas.drawPath(PHumanPiece, cPaint);
+                columnPieces[column]++;
+
+                if (i<gActivity.getAIMoves().size()) {
+                    action = gActivity.getAIMoves().get(i);
+                    column = action % 7;
+                    x = MARGINX + column * cWidth + cWidth / 2.0f;
+                    //y = distance;
+                    y = canvas.getHeight()-MARGINY-cWidth*columnPieces[column]-cWidth/distance;
+                    PAIPiece.addCircle(x,y, radius, Path.Direction.CW);
+                    cPaint.setColor(0xFFFF0000);
+                    canvas.drawPath(PAIPiece, cPaint);
+
+                    columnPieces[column]++;
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (gActivity.getAITurn()) return true;
+
+        float x = event.getX();
+        float y = event.getY();
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                break;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                if (y < MARGINY || x < MARGINX || y > endY || x > endX) return true;
+
+
+                int column = (int)((x-MARGINX)/cWidth);
+
+                for (int i=0; i<6; i++)
+                    if (gActivity.getBoard()[35+column-7*i] == 0) {
+                        gActivity.getBoard()[35+column-7*i] = game_activity.HUMAN_PIECE;
+                        gActivity.getHumanMoves().add(35+column-7*i);
+                        break;
+                    }
+
+                invalidate();
+                gActivity.setAiTurn();
+                if (gActivity.gameEnded(gActivity.getBoard())) {
+                    if (gActivity.aiWon(gActivity.getBoard()))
+                        gActivity.getTextView().setText("AI Won!");
+                    else if (gActivity.aiLost(gActivity.getBoard()))
+                        gActivity.getTextView().setText("You Won!");
+                    else if (gActivity.aiDraw(gActivity.getBoard()))
+                        gActivity.getTextView().setText("Draw");
+                    return true;
+                }
+                Thread thread = new Thread(gActivity);
+                thread.start();
+
+                break;
+            default:
+                return false;
+        }
+
+        return true;
+    }
+
+
+    public void drawBoard() {
+        cBitmap = Bitmap.createBitmap(cBitmap.getWidth(), cBitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        cCanvas = new Canvas(cBitmap);
+        CanvasPaint = new Paint(Paint.DITHER_FLAG);
+        cCanvas.drawBitmap(cBitmap, 0, 0, CanvasPaint);
+
+        setPathPaint();
+
+        invalidate();
+
+    }
+
+
+
+
+    }
--- app/src/main/java/edu/handong/android/connect4/game_activity.java
+++ app/src/main/java/edu/handong/android/connect4/game_activity.java
@@ -2,122 +2,411 @@ package edu.handong.android.connect4;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.TextView;
+import android.widget.ImageView;
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
+import java.util.HashMap;
+import java.util.Random;
+import java.util.Vector;
+import static java.lang.Math.exp;
+import static java.lang.Math.max;
 
-public class game_activity extends AppCompatActivity {
+
+public class game_activity extends AppCompatActivity implements Runnable{
+
+    private static final String MODEL_FILE = "file:///android_asset/alphazero19.pb";
+
+    private static final String INPUT_NODE = "main_input";
+    private static final String OUTPUT_NODE1 = "value_head/Tanh"; //To find out the exact output node names
+    private static final String OUTPUT_NODE2 = "policy_head/MatMul";
+
+    private ImageButton mButton;
+    private Connect4View mBoardView;
+    private TextView mTextView;
+    private ImageView conBoard;
+
+    public static final int AI_PIECE = -1;
+    public static final int HUMAN_PIECE = 1;
+    private static final int PIECES_NUM = 42;
+
+    private Boolean aiFirst = false;
+    private Boolean aiTurn = false;
+
+    private Vector<Integer> aiMoves = new Vector<>();
+    private Vector<Integer> humanMoves = new Vector<>();
+
+    private int board[] = new int[PIECES_NUM];
+    private static final HashMap<Integer, String> PIECE_SYMBOL;
+    static
+    {
+        PIECE_SYMBOL = new HashMap<Integer, String>();
+        PIECE_SYMBOL.put(AI_PIECE, "X");
+        PIECE_SYMBOL.put(HUMAN_PIECE, "O");
+        PIECE_SYMBOL.put(0, "-");
+    }
+
+    private TensorFlowInferenceInterface mInferenceInterface;
+
+    private final int winners[][] = {
+            {0,1,2,3},
+            {1,2,3,4},
+            {2,3,4,5},
+            {3,4,5,6},
+
+            {7,8,9,10},
+            {8,9,10,11},
+            {9,10,11,12},
+            {10,11,12,13},
+
+            {14,15,16,17},
+            {15,16,17,18},
+            {16,17,18,19},
+            {17,18,19,20},
+
+            {21,22,23,24},
+            {22,23,24,25},
+            {23,24,25,26},
+            {24,25,26,27},
+
+            {28,29,30,31},
+            {29,30,31,32},
+            {30,31,32,33},
+            {31,32,33,34},
+
+            {35,36,37,38},
+            {36,37,38,39},
+            {37,38,39,40},
+            {38,39,40,41},
+
+            // vertically
+            {0,7,14,21},
+            {7,14,21,28},
+            {14,21,28,35},
+
+            {1,8,15,22},
+            {8,15,22,29},
+            {15,22,29,36},
+
+            {2,9,16,23},
+            {9,16,23,30},
+            {16,23,30,37},
+
+            {3,10,17,24},
+            {10,17,24,31},
+            {17,24,31,38},
+
+            {4,11,18,25},
+            {11,18,25,32},
+            {18,25,32,39},
+
+            {5,12,19,26},
+            {12,19,26,33},
+            {19,26,33,40},
+
+            {6,13,20,27},
+            {13,20,27,34},
+            {20,27,34,41},
+
+            // diagonally
+            {3,9,15,21},
+
+            {4,10,16,22},
+            {10,16,22,28},
+
+            {5,11,17,23},
+            {11,17,23,29},
+            {17,23,29,35},
+
+            {6,12,18,24},
+            {12,18,24,30},
+            {18,24,30,36},
+            {13,19,25,31},
+            {19,25,31,37},
+            {20,26,32,38},
+
+            {3,11,19,27},
+
+            {2,10,18,26},
+            {10,18,26,34},
+
+            {1,9,17,25},
+            {9,17,25,33},
+            {17,25,33,41},
+
+            {0,8,16,24},
+            {8,16,24,32},
+            {16,24,32,40},
+            {7,15,23,31},
+            {15,23,31,39},
+            {14,22,30,38}};
+
+
+    public boolean getAITurn() {
+        return aiTurn;
+    }
+
+    public boolean getAIFirst() {
+        return aiFirst;
+    }
+
+    public Vector<Integer> getAIMoves() {
+        return aiMoves;
+    }
+
+    public Vector<Integer> getHumanMoves() {
+        return humanMoves;
+    }
+
+    public int[] getBoard() {
+        return board;
+    }
+
+    public void setAiTurn() {
+        aiTurn = true;
+    }
+
+
+
+    public boolean aiWon(int bd[]) {
+        for (int i=0; i<69; i++) {
+            int sum = 0;
+            for (int j=0; j<4; j++)
+                sum += bd[winners[i][j]];
+            if (sum == 4*AI_PIECE ) return true;
+        }
+        return false;
+    }
+
+    public boolean  aiLost(int bd[]) {
+        for (int i=0; i<69; i++) {
+            int sum = 0;
+            for (int j=0; j<4; j++)
+                sum += bd[winners[i][j]];
+            if (sum == 4*HUMAN_PIECE ) return true;
+        }
+        return false;
+    }
+
+    public boolean aiDraw(int bd[]) {
+        boolean hasZero = false;
+        for (int i=0; i<PIECES_NUM; i++) {
+            if (bd[i] == 0) {
+                hasZero = true;
+                break;
+            }
+        }
+        if (!hasZero) return true;
+        return false;
+    }
+
+
+    public boolean gameEnded(int[] bd) {
+        if (aiWon(bd) || aiLost(bd) || aiDraw(bd)) return true;
+
+        return false;
+    }
+
+    void getAllowedActions(int bd[], Vector<Integer> actions) {
+
+        for (int i=0; i<PIECES_NUM; i++) {
+            if (i>=PIECES_NUM-7) {
+                if (bd[i] == 0)
+                    actions.add(i);
+            }
+            else {
+                if (bd[i] == 0 && bd[i+7] != 0)
+                    actions.add(i);
+            }
+        }
+
+
+    }
+
+    public TextView getTextView() {
+        return mTextView;
+    }
+
+
+
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-        ImageButton game_Button = (ImageButton)findViewById(R.id.back_button);
-        game_Button.setOnClickListener(view -> {
-            Intent intent = new Intent(this, NewGame_Settings.class);
-            startActivity(intent);}
-        ); //comment 2
-
-        View column1 = findViewById(R.id.view1);
-        View column2 = findViewById(R.id.view2);
-        View column3 = findViewById(R.id.view3);
-        View column4 = findViewById(R.id.view4);
-        View column5 = findViewById(R.id.view5);
-        View column6 = findViewById(R.id.view6);
-        View column7 = findViewById(R.id.view7);
-
-        column1.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN: message(1);
-                        break;
-                }return true;
-            }
-
+        mButton = findViewById(R.id.reload_game_button);
+        mTextView = findViewById(R.id.textview);
+        mBoardView = findViewById(R.id.boardview);
+        ImageButton settings = findViewById(R.id.settings_button);
+        settings.setOnClickListener(view -> {
+            Intent intent = new Intent(this, SettingsActivity.class);
+            startActivity(intent);
         });
 
-        column2.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN: message(2);
-                        break;
-                }
-                return true;
-            }
-        });
+        mButton.setOnClickListener(view -> {
+            mTextView.setText("");
 
-        column3.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN: message(3);
-                        break;
-                }
-                return true;
-            }
-        });
+            Random rand = new Random();
+            int n = rand.nextInt(2);
 
-        column4.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN: message(4);
-                        break;
-                }
-                return true;
-            }
-        });
+            aiFirst = (n==0); // make this random between true and false
 
-        column5.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN: message(5);
-                        break;
-                }
-                return true;
-            }
-        });
+            if (aiFirst) aiTurn = true;
+            else aiTurn = false;
 
-        column6.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN: message(6);
-                        break;
-                }
-                return true;
-            }
-        });
+            if (aiTurn)
+                mTextView.setText("Waiting for AI's move");
+            else
+                mTextView.setText("Tap the column for your move");
 
-        column7.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent event) {
+            for (int i=0; i<PIECES_NUM; i++)
+                board[i] = 0;
+            aiMoves.clear();
+            humanMoves.clear();
+            mBoardView.drawBoard();
 
-                switch (event.getAction()) {
-                    case MotionEvent.ACTION_DOWN: message(7);
-                        break;
-                }
-                return true;
-            }
+            Thread thread = new Thread(game_activity.this);
+            thread.start();
         });
 
 
 
     }
+    @Override
+    public void run() {
 
+        final String result = playGame();
+
+        runOnUiThread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        mBoardView.invalidate();
+                        mTextView.setText(result);
+                    }
+                });
+    }
+
+    void softmax(float vals[], int count) {
+        float maxval = -Float.MAX_VALUE;
+        for (int i=0; i<count; i++) {
+            maxval = max(maxval, vals[i]);
+        }
+        float sum = 0.0f;
+        for (int i=0; i<count; i++) {
+            vals[i] = (float)exp(vals[i] - maxval);
+            sum += vals[i];
+        }
+        for (int i=0; i<count; i++) {
+            vals[i] /= sum;
+        }
+    }
+
+    void getProbs(int binary[], float probs[]) {
+        if (mInferenceInterface == null) {
+            AssetManager assetManager = getAssets();
+            mInferenceInterface = new TensorFlowInferenceInterface(assetManager, MODEL_FILE);
+        }
+
+        float[] floatValues  = new float[2*6*7];
+
+        for (int i=0; i<2*6*7; i++) {
+            floatValues[i] = binary[i];
+        }
+
+        float[] value = new float[1];
+        float[] policy = new float[42];
+
+        mInferenceInterface.feed(INPUT_NODE, floatValues, 1, 2, 6, 7);
+        mInferenceInterface.run(new String[] {OUTPUT_NODE1, OUTPUT_NODE2}, false);
+        mInferenceInterface.fetch(OUTPUT_NODE1, value);
+        mInferenceInterface.fetch(OUTPUT_NODE2, policy);
+
+        Vector<Integer> actions = new Vector<>();
+        getAllowedActions(board, actions);
+        for (int action : actions) {
+            probs[action] = policy[action];
+        }
+
+        softmax(probs, PIECES_NUM);
+    }
+
+    void printBoard(int bd[]) {
+        for (int i = 0; i<6; i++) {
+            for (int j=0; j<7; j++) {
+                System.out.print(" " + PIECE_SYMBOL.get(bd[i*7+j]));
+            }
+            System.out.println("");
+        }
+
+        System.out.println("\n\n");
+    }
+
+
+    String playGame() {
+        if (!aiTurn) return "Tap the column for your move";
+
+        int binary[] = new int[PIECES_NUM*2];
+
+        // convert board to binary input
+        for (int i=0; i<PIECES_NUM; i++)
+            if (board[i] == 1) binary[i] = 1;
+            else binary[i] = 0;
+
+        for (int i=0; i<PIECES_NUM; i++)
+            if (board[i] == -1) binary[42+i] = 1;
+            else binary[PIECES_NUM+i] = 0;
+
+        float probs[] = new float[PIECES_NUM];
+        for (int i=0; i<PIECES_NUM; i++)
+            probs[i] = -100.0f;
+        getProbs(binary, probs);
+        int action = -1;
+
+        float max = 0.0f;
+        for (int i=0; i<PIECES_NUM; i++) {
+            if (probs[i] > max) {
+                max = probs[i];
+                action = i;
+            }
+        }
+
+        board[action] = AI_PIECE;
+        printBoard(board);
+        aiMoves.add(action);
+
+        if (aiWon(board)) return "THE MACHINE BEATS YOU!";
+        else if (aiLost(board)) return "You Won!";
+        else if (aiDraw(board)) return "Draw";
+
+        aiTurn = false;
+        return "Tap the column for your move";
+
+    }
+
+
+
+}
+/*
     protected void message (int a){
         CharSequence text = "COLUMN "+a;
         int duration = Toast.LENGTH_SHORT;
         Toast toast = Toast.makeText(this,text,duration);
         toast.show();
     }
-}
+
+ */
+
